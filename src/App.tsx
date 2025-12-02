f640a4735a09b3ac6615e7a1d348182ceb542baf
DJoztin <al267187@edu.uaa.mx>
2025-12-02 12:36:21 -0600

    signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/logo.webp'
-
-import LoginSignup from './components/auth/LoginSignin'
+import { Route, Routes } from 'react-router-dom';
+import LoginSignin from './pages/LoginSignin';
+import Signup from './pages/Signup';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <LoginSignup />
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<LoginSignin />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
