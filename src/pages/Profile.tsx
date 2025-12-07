7a31eaa5a49ca14eed6614983765196b3b71c7ed
DJoztin <al267187@edu.uaa.mx>
2025-12-07 15:09:34 -0600

    useEffect profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { useSearchParams } from 'react-router-dom';
 import EditProfileModal from '../components/profile/EditProfileModal';
